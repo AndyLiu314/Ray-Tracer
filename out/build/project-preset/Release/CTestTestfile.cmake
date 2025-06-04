ee739a0197418355ecb4385a24e96a8ba6a20a31
AndyLiu314 <andyliu4002@gmail.com>
2025-06-04 15:02:49 -0700

    Deleted old build directory and cache * Reconfigured project with new settings to fully remove cmake testing

--- out/build/project-preset/Release/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andyliu/Projects/Ray-Tracer
-# Build directory: /home/andyliu/Projects/Ray-Tracer/out/build/project-preset/Release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
