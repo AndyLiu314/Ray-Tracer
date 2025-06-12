003b0db551de4a13602c6761baf7ebf6977071cf
AndyLiu314 <andyliu4002@gmail.com>
2025-06-12 13:35:54 -0700

    Added a sphere class * Used for calculating ray intersections with spheres

---
+++ utility/sphere.h
@@ -0,0 +1,45 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+    public:
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& record) const override {
+            vec3 oc = center - r.origin();
+            auto a = r.direction().length_squared();
+            auto h = dot(r.direction(), oc);
+            auto c = oc.length_squared() - radius*radius;
+            auto discriminant = h*h - a*c;
+
+            if (discriminant < 0) {
+                return false;
+            }
+
+            auto sqrt_discr = std::sqrt(discriminant);
+
+            // chooses the nearest root (i.e the nearest intersection point) first
+            auto root = (h - sqrt_discr) / a;
+            if (root <= ray_tmin || root >= ray_tmax) {
+                root = (h + sqrt_discr) / a;
+                if (root <= ray_tmin || root >= ray_tmax) {
+                    return false;
+                }
+            }
+
+            record.t = root;
+            record.p = r.at(record.t);
+            record.normal = (record.p - center) / radius; // divides by radius to normalize the normal vector
+
+            return true;
+        }
+
+    private:
+        point3 center;
+        double radius;
+};
+
+#endif
