38cee874be079cb12c8cca826c42876b5c37055c
AndyLiu314 <andyliu4002@gmail.com>
2025-06-19 02:32:28 -0700

    Modified main to use hittable list to store spheres

--- main.cpp
+++ main.cpp
@@ -4,25 +4,10 @@
 #include "geometry/hittable_list.h"
 #include "geometry/sphere.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = r.direction().length_squared();
-    auto h = dot(r.direction(), oc);
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = h*h - a*c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (h - std::sqrt(discriminant)) / a;
-    }
-}
-
-colour ray_colour(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * colour(N.x() + 1, N.y() + 1, N.z() + 1);
+colour ray_colour(const ray& r, const hittable_list& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + colour(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -40,6 +25,13 @@ int main() {
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
 
@@ -71,7 +63,7 @@ int main() {
             auto ray_direction = pixel_location - camera_center;
             ray r(camera_center, ray_direction);
 
-            colour pixel_colour = ray_colour(r);
+            colour pixel_colour = ray_colour(r, world);
             write_colour(std::cout, pixel_colour);
         }
     }
