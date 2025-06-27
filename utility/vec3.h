2918fde02d9a36bd21de926f22d7ea1311bddaa9
AndyLiu314 <andyliu4002@gmail.com>
2025-06-27 12:32:06 -0700

    Added simple diffuse material lighting

--- utility/vec3.h
+++ utility/vec3.h
@@ -41,6 +41,14 @@ class vec3 {
     double length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
 };
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
@@ -91,6 +99,25 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
+}
+
+inline vec3 random_unit_vector() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        auto len_sq = p.length_squared();
+        if (1e-160 < len_sq && len_sq <= 1) {
+            return p / sqrt(len_sq);
+        }
+    }
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 ran_vec = random_unit_vector();
+    if (dot(ran_vec, normal) > 0.0) {
+        return ran_vec;
+    } else {
+        return -ran_vec;
+    }
 }
 
 #endif
