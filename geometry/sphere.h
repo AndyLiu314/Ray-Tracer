2b2297e45571460db24313f84f210982b871ed0e
AndyLiu314 <andyliu4002@gmail.com>
2025-07-04 12:16:46 -0700

    Added data structure for storing material info

--- geometry/sphere.h
+++ geometry/sphere.h
@@ -5,7 +5,9 @@
 
 class sphere : public hittable {
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {
+            // TODO: Initialize the material pointer `material`.
+        }
 
         bool hit(const ray& r, interval ray_t, hit_record& record) const override {
             vec3 oc = center - r.origin();
@@ -33,6 +35,7 @@ class sphere : public hittable {
             record.p = r.at(record.t);
             vec3 outward_normal = (record.p - center) / radius; // Divides by radius to normalize the vector
             record.set_face_normal(r, outward_normal);
+            record.material = material;
 
             return true;
         }
@@ -40,6 +43,7 @@ class sphere : public hittable {
     private:
         point3 center;
         double radius;
+        shared_ptr<material> material;
 };
 
 #endif
