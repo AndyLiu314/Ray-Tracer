31849deb3c074069c2b1d48939289ba2f1d4eb07
AndyLiu314 <andyliu4002@gmail.com>
2025-07-11 00:18:02 -0700

    Implemented defocus blur

--- main.cpp
+++ main.cpp
@@ -18,8 +18,11 @@ int main() {
 
     cam.vfov = 45;
     cam.lookfrom = point3(-2,2,1);
-    cam.lookat   = point3(0,0,-1);
-    cam.vup      = vec3(0,1,0);
+    cam.lookat = point3(0,0,-1);
+    cam.vup = vec3(0,1,0);
+
+    cam.defocus_angle = 10.0;
+    cam.focus_dist = 3.4;
 
     // Materials 
     // auto material_center = make_shared<lambertian>(colour(0.6, 0.2, 0.2));
@@ -27,7 +30,7 @@ int main() {
     auto material_ground = make_shared<lambertian>(colour(0.3, 0.6, 0.3));
     auto material_left   = make_shared<metal>(colour(0.8, 0.8, 0.8), 0.1);
     auto material_right  = make_shared<metal>(colour(0.8, 0.6, 0.2), 1.0);
-    auto material_center   = make_shared<dielectric>(1.50);
+    auto material_center = make_shared<dielectric>(1.50);
     auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
 
     // World
