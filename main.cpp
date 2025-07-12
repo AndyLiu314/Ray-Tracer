42c27ff09215f44d43879eec3e2f93fe3d6fcf9b
AndyLiu314 <andyliu4002@gmail.com>
2025-07-12 15:48:30 -0700

    Added new scene images and changed scene layout

--- main.cpp
+++ main.cpp
@@ -8,26 +8,26 @@
 int main() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(colour(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // auto ground_material = make_shared<lambertian>(colour(0.5, 0.5, 0.5));
+    // world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            point3 center(a + 0.9*random_double(), 3.5*random_double(), b + 0.9*random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
 
-                if (choose_mat < 0.8) {
+                if (choose_mat < 0.45) {
                     // diffuse
                     auto albedo = colour::random() * colour::random();
                     sphere_material = make_shared<lambertian>(albedo);
                     world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
+                } else if (choose_mat < 0.90) {
                     // metal
                     auto albedo = colour::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
+                    auto fuzz = random_double(0, 0.7);
                     sphere_material = make_shared<metal>(albedo, fuzz);
                     world.add(make_shared<sphere>(center, 0.2, sphere_material));
                 } else {
@@ -52,15 +52,15 @@ int main() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 800;
-    cam.samples_per_pixel = 125;
+    cam.samples_per_pixel = 150;
     cam.max_recursion_depth = 30;
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
+    cam.vfov     = 45;
+    cam.lookfrom = point3(0,3,13);
     cam.lookat   = point3(0,0,0);
     cam.vup      = vec3(0,1,0);
 
-    cam.defocus_angle = 0.6;
+    cam.defocus_angle = 0.0;
     cam.focus_dist    = 10.0;
 
     cam.render(world);
