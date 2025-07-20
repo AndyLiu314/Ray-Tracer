02beb2c124a6df76b728eeb35b856079cbe75740
AndyLiu314 <andyliu4002@gmail.com>
2025-07-20 12:53:38 -0700

    Fixed indent and formatting inconsistencies

--- geometry/hittable.h
+++ geometry/hittable.h
@@ -4,27 +4,27 @@
 class material;
 
 class hit_record {
-  public:
-    point3 p;
-    vec3 normal;
-    shared_ptr<material> mat;
-    double t;
-    bool front_face;
+    public:
+        point3 p;
+        vec3 normal;
+        shared_ptr<material> mat;
+        double t;
+        bool front_face;
 
-    void set_face_normal(const ray& r, const vec3& outward_normal) {
-      // NOTE: the parameter `outward_normal` is assumed to have unit length
-      // Converts all outward normals to always point against the ray
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            // NOTE: the parameter `outward_normal` is assumed to have unit length
+            // Converts all outward normals to always point against the ray
 
-      front_face = dot(r.direction(), outward_normal) < 0;
-      normal = front_face ? outward_normal : -outward_normal;
-    }
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 
 class hittable {
-  public:
-    virtual ~hittable() = default;
+    public:
+        virtual ~hittable() = default;
 
-    virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+        virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 };
 
 #endif
